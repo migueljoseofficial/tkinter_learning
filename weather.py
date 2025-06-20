00551bccd4fd02c72944a494aa82f057bc753e52
migueljoseoffical <migueljoseofficial>
2025-06-20 14:18:06 -0500

    feet to meters conversion

--- weather.py
+++ weather.py
@@ -1,7 +1,12 @@
 from tkinter import *
 from tkinter import ttk
 
-# def calculate
+def calculate(*args):
+    try:
+        value = float(feet.get()) #gets the value from the feet ()
+        meters.set(int(value * .304 * 10000.0 +.5)/10000.0)
+    except ValueError:
+        pass
 
 
 #sets up the main application window
@@ -19,4 +24,26 @@ root.rowconfigure(0, weight=1)
 
 #entry widget
 feet = StringVar()
-feet_entry
+feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet) #parent is the first parameter for a widget.
+
+feet_entry.grid(column=2, row=1, sticky=(W,E)) #sticky says which walls it should attach t (west , east) or left and right
+
+#meters widget 
+meters = StringVar()
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+
+ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
+
+ttk.Label(mainframe, text='feet').grid(column=3, row=1, sticky=W)
+ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+
+
+#could loop through all the children of a frame/widget
+for children in mainframe.winfo_children():
+    print(children)
+    children.grid_configure(padx=5, pady=5)
+
+feet_entry.focus()
+root.bind("<Return>", calculate)
+root.mainloop()
