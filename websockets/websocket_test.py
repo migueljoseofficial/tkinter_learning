bbd291683fbe35460fd65caaaea9ef7a7325d48b
migueljoseofficial <miguel_maninang@yahoo.com>
2025-06-23 14:09:25 -0500

    tried to update the test, but still an error

--- websockets/websocket_test.py
+++ websockets/websocket_test.py
@@ -2,7 +2,11 @@ import asyncio
 import websockets
 import yaml
 #gets the proto file for logging in
-from proto import login_pb2
+import sys, os
+sys.path.insert(0, os.path.abspath("proto"))
+
+from proto.t4.v1.auth import auth_pb2
+
 #port443
 
 #heartbeat connection every 20 seconds (only have 3 consecutive heratbeats to send before the connection is termianted)
@@ -19,17 +23,17 @@ def load_config(path="config/config.yaml"):
 async def connect_with_auth(ws, config): #arguments are the websocket and the config file
      
      #uses proto file to sent out
-     login = login_pb2.LoginRequest(
+     login = auth_pb2.LoginRequest(
           firm= config["websocket"]["firm"],
           username=config["websocket"]["username"],
           password= config["websocket"]["password"],
           app_name= config["websocket"]["app_name"],
           app_license= config["websocket"]["app_license"],
-          priceFormat = config["websocket"]["priceFormat"]
+          price_format = config["websocket"]["priceFormat"]
      )
      
      ##must send the information using the protocol buffers not json.
-     await ws.send(login)
+     await ws.send(login.SerializeToString())
      response = await ws.recv()
      print(response)
 
